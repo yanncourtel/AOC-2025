114ae9aaffbcca947f17d89f10dd2359091eb8e9
Yann Courtel <courtel.yann@gmail.com>
2026-01-20 13:05:37 +0100

    refactor: separate the creation of calculated invoice with extension bridge.

---
+++ exercises/day19-North-Pole-Companies/csharp/NorthPole/CalculatedInvoiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace NorthPole;
+
+public static class CalculatedInvoiceExtensions
+{
+    private static readonly IInvoiceFormatter TextInvoiceFormatter = new TextInvoiceFormatter();
+    private static readonly IInvoiceFormatter TextInvoiceWithTaxFormatter = new TextInvoiceWithTaxFormatter();
+
+    public static string Print(this CalculatedInvoice invoice) 
+        => TextInvoiceFormatter.Format(invoice);
+
+    public static string PrintWithTaxes(this CalculatedInvoice invoice) 
+        => TextInvoiceWithTaxFormatter.Format(invoice);
+}
--- exercises/day19-North-Pole-Companies/csharp/NorthPole/InvoicePrinter.cs
+++ exercises/day19-North-Pole-Companies/csharp/NorthPole/InvoicePrinter.cs
@@ -5,50 +5,14 @@ namespace NorthPole;
 
 public class InvoicePrinter
 {
-    private readonly IInvoiceFormatter _formatter;
-    private readonly IInvoiceFormatter _formatterWithTax;
-
-    public InvoicePrinter() 
-        : this(
-            new TextInvoiceFormatter(), 
-            new TextInvoiceWithTaxFormatter())
-    {
-    }
-
-    private InvoicePrinter(
-        IInvoiceFormatter formatter, 
-        IInvoiceFormatter formatterWithTax)
-    {
-        _formatter = formatter;
-        _formatterWithTax = formatterWithTax;
-    }
-    
     // Legacy methods - kept for backward compatibility
     public string Print(Invoice invoice, Dictionary<string, ElfCompany> elfCompanies) 
-        => Print(
-            EnrichInvoice(invoice, elfCompanies));
+        => EnrichInvoice(invoice, elfCompanies)
+            .Print();
 
-    private string Print(EnrichedInvoice invoice) 
-        => _formatter.Format(
-            CalculateInvoice(invoice, includeTax: false));
-
-    public string PrintWithTaxes(Invoice invoice, Dictionary<string, ElfCompany> elfCompanies) 
-        => PrintWithTaxes(
-            EnrichInvoice(invoice, elfCompanies));
-
-    private string PrintWithTaxes(EnrichedInvoice invoice) 
-        => _formatterWithTax.Format(
-            CalculateInvoice(invoice, includeTax: true));
-
-    private CalculatedInvoice CalculateInvoice(EnrichedInvoice invoice, bool includeTax) 
-        => CalculatedInvoice.From(invoice.Customer, invoice.Deliveries
-            .Select(d => CalculateInvoiceLine(d, includeTax))
-            .ToList());
-
-    private InvoiceLine CalculateInvoiceLine(EnrichedDelivery enriched, bool includeTax) 
-        => includeTax
-            ? InvoiceLine.LineWithTaxFor(enriched)
-            : InvoiceLine.LineFor(enriched);
+    public string PrintWithTaxes(Invoice invoice, Dictionary<string, ElfCompany> elfCompanies)
+        => EnrichInvoice(invoice, elfCompanies)
+            .PrintWithTaxes();
 
     private static EnrichedInvoice EnrichInvoice(Invoice invoice, Dictionary<string, ElfCompany> elfCompanies) 
         => new(
